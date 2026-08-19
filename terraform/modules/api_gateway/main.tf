c19ac6c57f6a969f4e6260d4036e262aa0ec5c31
LucasDonizeti <lucasdonizete11@gmail.com>
2026-08-19 16:11:34 -0300

    fix : target group

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -41,13 +41,13 @@ resource "aws_apigatewayv2_vpc_link" "eks_vpc_link" {
   subnet_ids         = var.private_subnets
 }
 
-# 4. Target Group
+# 4. Target Group alterado para o tipo IP
 resource "aws_lb_target_group" "eks_nodes_tg" {
   name        = "${var.name}-eks-tg"
   port        = 30080
   protocol    = "TCP"
   vpc_id      = var.vpc_id
-  target_type = "instance"
+  target_type = "ip"
 
   health_check {
     protocol = "TCP"
@@ -57,7 +57,22 @@ resource "aws_lb_target_group" "eks_nodes_tg" {
   tags = var.tags
 }
 
-# 5. NLB Interno
+# 5. Busca automática das subnets privadas para extrair o CIDR/IPs
+data "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  id    = var.private_subnets[count.index]
+}
+
+# 6. Registra os blocos de IP/interfaces das subnets privadas no Target Group
+resource "aws_lb_target_group_attachment" "eks_ip_attachment" {
+  count            = length(data.aws_subnet.private)
+  target_group_arn = aws_lb_target_group.eks_nodes_tg.arn
+  # Utiliza o primeiro IP util da subnet privada onde os Nodes/Pods residem
+  target_id        = cidrhost(data.aws_subnet.private[count.index].cidr_block, 10) 
+  port             = 30080
+}
+
+# 7. NLB Interno
 resource "aws_lb" "internal_nlb" {
   name               = "${var.name}-nlb"
   internal           = true
@@ -67,7 +82,7 @@ resource "aws_lb" "internal_nlb" {
   tags = var.tags
 }
 
-# 6. Listener do NLB
+# 8. Listener do NLB
 resource "aws_lb_listener" "internal_nlb_listener" {
   load_balancer_arn = aws_lb.internal_nlb.arn
   port              = 80
@@ -79,7 +94,7 @@ resource "aws_lb_listener" "internal_nlb_listener" {
   }
 }
 
-# 7. Integração com VPC Link
+# 9. Integração com VPC Link
 resource "aws_apigatewayv2_integration" "eks_integration" {
   api_id             = aws_apigatewayv2_api.main_gateway.id
   integration_type   = "HTTP_PROXY"
@@ -89,14 +104,14 @@ resource "aws_apigatewayv2_integration" "eks_integration" {
   connection_id      = aws_apigatewayv2_vpc_link.eks_vpc_link.id
 }
 
-# 8. Rota genérica
+# 10. Rota genérica
 resource "aws_apigatewayv2_route" "eks_route" {
   api_id    = aws_apigatewayv2_api.main_gateway.id
   route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.eks_integration.id}"
 }
 
-# 9. Stage Padrão
+# 11. Stage Padrão
 resource "aws_apigatewayv2_stage" "default_stage" {
   api_id      = aws_apigatewayv2_api.main_gateway.id
   name        = "$default"
