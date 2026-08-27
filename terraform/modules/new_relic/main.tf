58e009f7e32f676619f949c47a76f07b2fb0c8d4
LucasDonizeti <lucasdonizete11@gmail.com>
2026-08-27 15:26:55 -0300

    remover role policy attachment

--- terraform/modules/new_relic/main.tf
+++ terraform/modules/new_relic/main.tf
@@ -2,21 +2,3 @@
 data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
-
-# 1. Política de leitura geral de métricas do CloudWatch / Infra
-resource "aws_iam_role_policy_attachment" "newrelic_read_only" {
-  role       = data.aws_iam_role.lab_role.name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-
-# 2. Permissão de acesso direto às métricas do Amazon RDS
-resource "aws_iam_role_policy_attachment" "newrelic_rds" {
-  role       = data.aws_iam_role.lab_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
-}
-
-# 3. Permissão de acesso direto às métricas das Funções AWS Lambda
-resource "aws_iam_role_policy_attachment" "newrelic_lambda" {
-  role       = data.aws_iam_role.lab_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_ReadOnlyAccess"
-}
